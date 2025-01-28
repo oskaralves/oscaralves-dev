05fca1118639c9e164507674bd301bea07e3bf4c
Oscar Alves <osk.alves@gmail.com>
2025-01-28 01:41:48 -0300

    config: configuration of project

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.simpleicons.org',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '/**',
+      },
+    ],
+  },
   /* config options here */
 };
 
