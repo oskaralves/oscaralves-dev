b5f51c870d23d265aeef6fb5499abcfe9bde0850
Oscar Alves <osk.alves@gmail.com>
2025-01-29 19:04:56 -0300

    chore: upgrade segments constant

--- src/constants/segments.tsx
+++ src/constants/segments.tsx
@@ -1,3 +1,5 @@
+import { getDictionary } from '@/dictionaries';
+import { Locale } from '@/types/locale';
 import {
   CloudServerIcon,
   DatabaseIcon,
@@ -8,67 +10,68 @@ import {
   WebDesign02Icon,
 } from 'hugeicons-react';
 
-export const segments = [
-  {
-    id: 'frontend',
-    icon: (
-      <WebDesign02Icon className="h-10 w-10 text-primary" strokeWidth={0.5} />
-    ),
-    title: 'Front end',
-    description:
-      'Criação de interfaces de usuário dinâmicas, acessíveis e responsivas, com foco em desempenho, manutenção eficiente e experiência intuitiva para os usuários.',
-  },
-  {
-    id: 'backend',
-    icon: (
-      <SourceCodeIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />
-    ),
-    title: 'Back end',
-    description:
-      'Desenvolvimento de sistemas robustos, escaláveis e seguros, garantindo eficiência, modularidade e integração fluida entre serviços e dados.',
-  },
-  {
-    id: 'mobile',
-    icon: (
-      <MobileProgramming01Icon
-        className="h-10 w-10 text-primary"
-        strokeWidth={0.5}
-      />
-    ),
-    title: 'Mobile',
-    description:
-      'Desenvolvimento de aplicativos mobile modernos e performáticos utilizando tecnologias líderes que me permitem a criação de experiências nativas e responsivas para projetos híbridos, garantindo flexibilidade e qualidade em soluções multiplataforma.',
-  },
-  {
-    id: 'test',
-    icon: (
-      <TestTube01Icon className="h-10 w-10 text-primary" strokeWidth={0.5} />
-    ),
-    title: 'Test',
-    description:
-      'Experiência em criação e implementação de testes automatizados para garantir a qualidade e confiabilidade do código. Conhecimento em ferramentas e bibliotecas voltadas para testes unitários, de integração e end-to-end, promovendo estabilidade e confiança no ciclo de desenvolvimento.',
-  },
-  {
-    id: 'database',
-    icon: <DatabaseIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />,
-    title: 'Database',
-    description:
-      'Conhecimento avançado no design, gerenciamento e otimização de bancos de dados relacionais e não relacionais, garantindo desempenho, segurança e escalabilidade no armazenamento e recuperação de dados.',
-  },
-  {
-    id: 'devops',
-    icon: (
-      <CloudServerIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />
-    ),
-    title: 'DevOps',
-    description:
-      'Implementação de práticas e ferramentas que conectam desenvolvimento e operações, promovendo automação, integração contínua, entrega rápida e monitoramento eficiente para garantir sistemas confiáveis e de alto desempenho.',
-  },
-  {
-    id: 'libs',
-    icon: <PackageIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />,
-    title: 'Principais Libs',
-    description:
-      'Especialização em bibliotecas amplamente utilizadas no mercado, essenciais para otimizar fluxos de trabalho, lidar com dados, gerenciar estados, integrar funcionalidades avançadas em projetos modernos, etc.',
-  },
-];
+export const getSegments = (locale: Locale = 'pt-BR') => {
+  const { FRONT_END, BACK_END, MOBILE, TEST, DATABASE, DEVOPS, LIBS } =
+    getDictionary(locale, 'segment');
+  return [
+    {
+      id: 'frontend',
+      icon: (
+        <WebDesign02Icon className="h-10 w-10 text-primary" strokeWidth={0.5} />
+      ),
+      title: FRONT_END.TITLE,
+      description: FRONT_END.DESCRIPTION,
+    },
+    {
+      id: 'backend',
+      icon: (
+        <SourceCodeIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />
+      ),
+      title: BACK_END.TITLE,
+      description: BACK_END.DESCRIPTION,
+    },
+    {
+      id: 'mobile',
+      icon: (
+        <MobileProgramming01Icon
+          className="h-10 w-10 text-primary"
+          strokeWidth={0.5}
+        />
+      ),
+      title: MOBILE.TITLE,
+      description: MOBILE.DESCRIPTION,
+    },
+    {
+      id: 'test',
+      icon: (
+        <TestTube01Icon className="h-10 w-10 text-primary" strokeWidth={0.5} />
+      ),
+      title: TEST.TITLE,
+      description: TEST.DESCRIPTION,
+    },
+    {
+      id: 'database',
+      icon: (
+        <DatabaseIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />
+      ),
+      title: DATABASE.TITLE,
+      description: DATABASE.DESCRIPTION,
+    },
+    {
+      id: 'devops',
+      icon: (
+        <CloudServerIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />
+      ),
+      title: DEVOPS.TITLE,
+      description: DEVOPS.DESCRIPTION,
+    },
+    {
+      id: 'libs',
+      icon: (
+        <PackageIcon className="h-10 w-10 text-primary" strokeWidth={0.5} />
+      ),
+      title: LIBS.TITLE,
+      description: LIBS.DESCRIPTION,
+    },
+  ];
+};
