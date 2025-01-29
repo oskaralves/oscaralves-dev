047de25b8362f2b83190f312af8a3d07e2a53912
Oscar Alves <osk.alves@gmail.com>
2025-01-29 19:04:40 -0300

    chore: created features cards section

---
+++ src/app/sections/projects.tsx
@@ -0,0 +1,10 @@
+export const ProjectsSection = () => {
+  return (
+    <section
+      id="projects"
+      className="flex min-h-[600px] w-full flex-col items-center justify-center bg-violet-500 p-4 md:h-[calc(100vh/2)] md:min-h-[calc(100vh-68px)] md:flex-row md:p-10"
+    >
+      <h1 className="text-5xl text-white">projects</h1>
+    </section>
+  );
+};
