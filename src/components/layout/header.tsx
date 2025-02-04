5a2b05b4e386abe96de27c2ac15c1db8b6208aa0
Oscar Alves <osk.alves@gmail.com>
2025-02-04 13:46:27 -0300

    fix: added transition-all in effect on header and sidebar expanded

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -17,7 +17,7 @@ export function Header() {
     <header className="sticky top-0 z-50 col-span-full flex h-[68px] items-center px-3 transition-all">
       <div
         className={cn(
-          'fixed left-0 top-[68px] -z-20 h-6 w-6 text-[#d6d6d6] dark:text-[#212121]',
+          'fixed left-0 top-[68px] -z-20 h-6 w-6 text-[#d6d6d6] transition-all dark:text-[#212121]',
           {
             'md:left-[246px]': sidebarExpanded,
             'md:left-[78px]': !sidebarExpanded,
