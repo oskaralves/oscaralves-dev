9f6c800d1e6078bf1e463152fedf296890283b5f
Oscar Alves <osk.alves@gmail.com>
2025-02-05 00:54:25 -0300

    chore: revert my email

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,6 +12,6 @@ export const APP_TITLE = `${PREFIX_KEY !== 'oa' ? '[DEV] ' : ''}Oscar Alves`;
 export const APP_LOCALE_KEY = `${PREFIX_KEY}.app-locale`;
 export const APP_SIDEBAR_EXPANDED = `${PREFIX_KEY}.sidebar-expanded`;
 
-export const MY_EMAIL = 'index@oscaralves.dev';
+export const MY_EMAIL = 'osk.alves@gmail.com';
 export const MY_LINKEDIN = 'linkedin.com/in/oskaralves';
 export const MY_GITHUB = 'github.com/oskaralves';
